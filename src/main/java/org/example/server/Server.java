b4598f67932790a0a65a0d6756e9f80236806502
Pragalv Bhattarai <bpragalv@gmail.com>
2026-01-19 20:55:04 -0500

    updates

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -1,18 +1,23 @@
 package org.example.server;
 
-public abstract class GenericServer {
+public abstract class Server {
     public static int serverIdCounter = 0;
     public static int ipAddressCounter = 1000;
 
+    protected String serverType;
     protected int serverId;
     protected int ipAddress;
 
-    GenericServer(){
+    Server(){
         this.serverId = serverIdCounter++;
         this.ipAddress = ipAddressCounter++;
     }
 
-    abstract void getMetrics();
+    public void getMetrics(){
+        System.out.println("Server Type: " + this.serverType);
+        System.out.println("Server IP Address: " + this.ipAddress);
+        System.out.println("Server ID: " + this.serverId);
+    };
     abstract void getHealth();
     abstract void getStatus();
 }
